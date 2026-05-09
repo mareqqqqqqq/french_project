a7da4b48a9b27e5460f15493e2b8b98ed2262867
mareqqqqqqq <markpopov646@gmail.com>
2026-05-09 19:27:08 +0700

    вернул jwt

--- app/backend/core/security.py
+++ app/backend/core/security.py
@@ -1,7 +1,14 @@
 from passlib.context import CryptContext
 import bcrypt
+import jwt
+from datetime import datetime, timedelta
+from app.backend.db import Settings
+import os
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHMS = ["HS256"]
 
 def get_password_hash(password: str):
     salt = bcrypt.gensalt()
@@ -14,3 +21,20 @@ def verify_password_hash(password: str, hashed_password: str):
         password.encode("utf-8"),
         hashed_password.encode("utf-8")
     )
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHMS[0])
+    return encoded_jwt
+
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(days=7)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHMS[0])
+    return encoded_jwt
