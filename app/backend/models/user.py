f1bc0b4d87e3b6bf54d99de8a5daf9e0a77c84cb
mareqqqqqqq <markpopov646@gmail.com>
2026-05-12 15:41:22 +0700

    add alembic versions

--- app/backend/models/user.py
+++ app/backend/models/user.py
@@ -5,12 +5,11 @@ from app.backend.db import Base
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True) # уникальность, не может быть 2 пользователя с одним и тем же id
+    id = Column(
+        Integer, primary_key=True
+    )  # уникальность, не может быть 2 пользователя с одним и тем же id
     username = Column(String(15), unique=True)
     email = Column(String(150), unique=True, index=True)
     hashed_password = Column(String(255))
-    created_at = Column(DateTime, server_default = func.now())
-
-
-
-
+    refresh_token = Column(String(500), nullable=True)
+    created_at = Column(DateTime, server_default=func.now())
